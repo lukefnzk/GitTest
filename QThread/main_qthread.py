13b81cf3a630b533e17de765df4da2fe19f9750a
lukefnzk12 <lukefnzk12@gmail.com>
2025-03-14 14:03:16 +0900

    25.03.14: signal / slot + QThread studing

--- QThread/main_qthread.py
+++ QThread/main_qthread.py
@@ -8,12 +8,37 @@ from threading import Thread, Event
 
 
 class ThreadWorker(QThread):
-    progressSignal = Signal(int)
+    progressSignal_num = Signal(int)
+    progressSignal_alpa = Signal(str)
+    
+    def __init__(self, mode='number'):
+        super().__init__()
+        self.mode = mode
+        self._is_running = True
     
     def run(self):
-        for i in range(1, 101):
-            self.progressSignal.emit(i)
+        if self.mode == 'number':
+            self.progress_num()
+        else:
+            self.progress_alpa()
+    
+    def stop(self):
+        self._is_running = False
+    
+    def progress_num(self):
+        for n in range(1, 31):
+            if not self._is_running:
+                break
+            self.progressSignal_num.emit(n)
             time.sleep(1.0)
+            
+    def progress_alpa(self):
+        alpa = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        for c in alpa:
+            if not self._is_running:
+                break
+            self.progressSignal_alpa.emit(c)
+            time.sleep(1.3)
     
 
 
@@ -26,6 +51,9 @@ class MainWindow(QMainWindow):
         self.model = QStandardItemModel()
         self.ui.listView.setModel(self.model)
         
+        # 쓰레드 객체를 저장할 리스트
+        self.thread_workers = []
+        
         self.setup_connections()
       
     def setup_connections(self):
@@ -33,17 +61,38 @@ class MainWindow(QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.startThread_2)
         
     def startThread_1(self):
-        self.ThreadWorker = ThreadWorker()
-        self.ThreadWorker.progressSignal.connect(self.update_progress)
-        self.ThreadWorker.start()
-        # self.fill_list()
+        worker = ThreadWorker(mode='number')
+        worker.progressSignal_num.connect(self.update_progress)
+        worker.finished.connect(lambda: self.cleanup_thread(worker))  # 쓰레드 완료시 정리
+        self.thread_workers.append(worker)
+        worker.start()
     
     def startThread_2(self):
-        self.model.clear()
+        worker = ThreadWorker(mode='alpha')
+        worker.progressSignal_alpa.connect(self.update_progress)
+        worker.finished.connect(lambda: self.cleanup_thread(worker))  # 쓰레드 완료시 정리
+        self.thread_workers.append(worker)
+        worker.start()
     
+    def cleanup_thread(self, worker):
+        if worker in self.thread_workers:
+            self.thread_workers.remove(worker)
+            worker.deleteLater()  # Qt 객체 정리
+    
+    def closeEvent(self, event):
+        # 프로그램 종료시 모든 쓰레드 정리
+        for worker in self.thread_workers:
+            worker.stop()  # 먼저 쓰레드에게 중지 신호를 보냄
+            worker.quit()
+            # 최대 1초만 기다림
+            if not worker.wait(500):
+                worker.terminate()  # 1초 후에도 종료되지 않으면 강제 종료
+        super().closeEvent(event)
+        
     def update_progress(self, value):
         self.model.appendRow(QStandardItem(str(value)))
-    
+        # 새 항목이 추가된 후 ListView를 최하단으로 스크롤
+        self.ui.listView.scrollToBottom()
     
     # def fill_list(self):
     #     n = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
